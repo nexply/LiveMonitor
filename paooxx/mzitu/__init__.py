03c761de7bcea41faf46e5580578346e443c4156
mumushuimei <350615774@qq.com>
2017-11-29 00:12:57 +0800

    更新

---
+++ paooxx/mzitu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Created on 17/11/29
+__author__ = 'Mumushuimei'
+import sys
+
+code = sys.getfilesystemencoding()
+
+if __name__ == '__main__':
+    pass
